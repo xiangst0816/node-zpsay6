09225c6d9319568ebf3a72a0840cf580481bd1bb
xiangst0816 <xiangst0816@gmail.com>
2022-10-16 11:46:15 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# node-zpsay6
+
+[Edit in Codeflow ⚡️](https://stackblitz.com/~/github.com/xiangst0816/node-zpsay6)
---
+++ index.js
@@ -0,0 +1,18 @@
+// run `node index.js` in the terminal
+
+console.log(`Hello Nod2e.js v${process.versions.node}!`);
+
+debugger;
+console.log(
+  import('./index.js').then((i) => {
+    debugger;
+    console.log(i);
+  })
+);
+console.log(`Hello Nod2e.js v${process.versions.node}!`);
+
+console.log(`Hello Nod2e.js v${process.versions.node}!`);
+debugger;
+
+console.log(`Hello Nod2e.js v${process.versions.node}!`);
+debugger;
